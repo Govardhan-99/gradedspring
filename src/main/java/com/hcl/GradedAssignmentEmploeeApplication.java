d1080fd8b96be1bda5e6baa2abe9cfc1716fd030
Govardhan-99 <84893634+Govardhan-99@users.noreply.github.com>
2021-06-01 16:04:53 +0530

    Update GradedAssignmentEmploeeApplication.java

--- src/main/java/com/hcl/GradedAssignmentEmploeeApplication.java
+++ src/main/java/com/hcl/GradedAssignmentEmploeeApplication.java
@@ -2,12 +2,16 @@ package com.hcl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
-public class GradedAssignmentEmploeeApplication {
+public class GradedAssignmentEmploeeApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradedAssignmentEmploeeApplication.class, args);
 	}
-
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(GradedAssignmentEmploeeApplication.class);
+    }
 }
