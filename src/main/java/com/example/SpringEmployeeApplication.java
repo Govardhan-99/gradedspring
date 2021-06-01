d737b6c2c6d37fc270db3983f1d17c2dd55cbcee
Govardhan-99 <84893634+Govardhan-99@users.noreply.github.com>
2021-06-01 17:20:43 +0530

    Update SpringEmployeeApplication.java

--- src/main/java/com/example/SpringEmployeeApplication.java
+++ src/main/java/com/example/SpringEmployeeApplication.java
@@ -3,6 +3,8 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import com.example.bins.Employee;
 import com.example.bins.Teacher;
@@ -10,7 +12,7 @@ import com.example.bins.Teacher;
 @SpringBootApplication
 public class SpringEmployeeApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) extends SpringBootServletInitializer {
 		ConfigurableApplicationContext context = SpringApplication.run(SpringEmployeeApplication.class, args);
 //		Employee e =context.getBean(Employee.class);
 //		Teacher t=context.getBean(Teacher.class);
@@ -19,5 +21,9 @@ public class SpringEmployeeApplication {
 //		System.out.println(t.hashCode());
 //		System.out.println(t.hashCode());
 	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(SpringbootFoodApplication.class);
+    }
 
 }
