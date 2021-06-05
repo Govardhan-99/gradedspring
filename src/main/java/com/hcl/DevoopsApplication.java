0bb6ba5d2ad591b1e58f591369ed0e02224aa79f
Govardhan-99 <relicuus99@gmail.com>
2021-06-05 10:38:33 +0530

    1st commit

---
+++ src/main/java/com/hcl/DevoopsApplication.java
@@ -0,0 +1,15 @@
+package com.hcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoopsApplication.class, args);
+		System.out.println("******** HELLO DEVOPS *************");
+
+	}
+
+}
