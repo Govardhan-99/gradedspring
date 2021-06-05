8eb33038dddaa5d86d433d5ddba38d2d7f315340
Govardhan-99 <relicuus99@gmail.com>
2021-06-05 10:19:51 +0530

    Devopps 1st commit

---
+++ src/test/java/com/hcl/SpringEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
